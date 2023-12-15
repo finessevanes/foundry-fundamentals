3519a7ed0ba98d97b74b4611fa7d19d675e69a1c
Vanessa Mercado <vanessa.mercado24@gmail.com>
2023-12-14 21:28:52 -0700

    section 8

--- foundry-FundMe-f23/test/FundMeTest.t.sol
+++ foundry-FundMe-f23/test/FundMeTest.t.sol
@@ -4,25 +4,26 @@ pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import "forge-std/console.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 
-    // function testOwnerIsMessageSender() public {
-    //     console.log(fundMe.i_owner());
-    //     assertEq(fundMe.i_owner(), msg.sender);
-    // }
-
     function testOwnerIsMessageSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        // assertEq(fundMe.i_owner(), address(this));
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        assertEq(fundMe.i_owner(), msg.sender);
+
     }
 
     function testPriceFeedVersionIsAccurate() public {
